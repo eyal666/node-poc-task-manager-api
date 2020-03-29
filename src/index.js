59e7d2ee74f5e3ec704e579e95fd65781c74b4ea
Eyal Roman <eyal.roman@gmail.com>
2020-03-29 16:51:18 +0300

    Added test suits

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
+const app = require('./app')
 const { port } = require('../config/config')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
